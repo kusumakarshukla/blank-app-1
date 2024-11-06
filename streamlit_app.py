4f2c0856104ef03fa93ef479c601b47cdf7d1a9a
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2024-11-06 15:11:13 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("This is my First StreamList APP")
+st.title("This is my First Streamlit APP")
 st.write(
     "I will be using StreamLit to deploy my machine learning Applications to showcase my work"
 )
