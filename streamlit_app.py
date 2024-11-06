fddf53e3a85e869c178726be609a7a20a534b9ab
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2024-11-06 15:38:25 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import datetime
 st.title("My App")
-@st.fragment(run_every="5s")
 def auto_function():
 		# This will update every 10 seconds!
 		df = str(datetime.datetime.now().strftime("%d-%m-%y %h:%M:%s"))
