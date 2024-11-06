1097fd9211c0706c224c6c3bf733c8ec6d0e655f
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2024-11-06 15:55:46 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,10 @@
 import streamlit as st
 import datetime
-@st.fragment(run_every="1s")
+#@st.fragment(run_every="1s")
 def auto_function():
-		seconds=int(datetime.datetime.now().second)
-		if seconds%5==0:
-			st.title("Asmit Stand!")
-		elif seconds%8==0:
-			st.title("Asmit Jump!")
-		elif seconds %7==0:
-			st.title("Asmit Sit")
-		else:
-			st.title("Asmit Smile")
+		
+		
+		st.title("Asmit Smile")
 			
 		
 
