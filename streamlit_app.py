e031316a91068d8fdcd680668ec61a8451210f4c
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2024-11-06 15:33:32 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 st.title("My App")
-st.write(
-    "github.com/kusumakarshukla"
-)
+@st.fragment(run_every="10s")
+def auto_function():
+		# This will update every 10 seconds!
+		df = get_latest_updates()
+		st.line_chart(df)
+
+auto_function()
