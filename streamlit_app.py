d03f30f0166af83909421826cdac96b634f3d874
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2024-11-06 15:45:24 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import datetime
 st.title("Asmit Exercise")
-@st.fragment(run_every="10s")
+@st.fragment(run_every="1s")
 def auto_function():
-		seconds=datetime.datetime.now().second
+		seconds=int(datetime.datetime.now().second)
 		if seconds%5==0:
 			st.write("Asmit Stand!")
 		if seconds%10==0:
