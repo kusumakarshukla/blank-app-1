348e3aa767aa1fc39d598ce5d784327c23cfe4d3
Kusumakar Shukla <kusumakar.shukla@gmail.com>
2024-11-06 15:43:50 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,9 @@ def auto_function():
 		seconds=datetime.datetime.now().second
 		if seconds%5==0:
 			st.write("Asmit Stand!")
-		if second%10==0:
+		if seconds%10==0:
 			st.write("Asmit Jump!")
-		if second %7==0:
+		if seconds %7==0:
 			st.write("Asmit Sit")
 			
 		
